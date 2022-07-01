e6766ca6c57871796215e346eb4f3101a2ef12fa
“Ruq23” <daruq20@gmail.com>
2022-07-01 02:17:29 -0700

    optionals

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/*
+ * main - prints all possible different combinations of three digits
+ * numbers must be separated by ,, followed by a space
+ * Return 0 always
+*/
+
+int main(void)
+{
+int ch;
+int n;
+int p;
+for (ch = 48; ch <= 57; ch++)
+{
+for (n = 49; n <= 57; n++)
+{
+for (p = 50; = <= 57; p++)
+{
+if (n > c && p > n)
+{
+putchar (ch);
+putchar (n);
+putchar(p);
+if (ch != 55 || n != 56 || p != 57)
+{
+putchar(44);
+putchar(32);
+}
+}
+}
+}
+}
+
+return (0);
+}
