d2dfcfaf78479a79bee41c76f2b9be94b0367894
“Ruq23” <daruq20@gmail.com>
2022-07-07 07:05:20 -0700

    fix

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -17,9 +17,9 @@ void print_diagonal(int n)
 		for (x = 0; x < n; x++)
 		{
 			for (i = 0; i < x; i++)
-				_putchar(32);
+				_putchar(' ');
 			_putchar(92);
-			_putchar('\n')
+			_putchar('\n');
 		}
 	}
 }
