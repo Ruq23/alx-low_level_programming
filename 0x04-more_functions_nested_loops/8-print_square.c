94328ee32376824a874c6e14511afe72826a3d4c
“Ruq23” <daruq20@gmail.com>
2022-07-07 06:22:55 -0700

    hot fix

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,20 +8,20 @@
 
 void print_square(int size)
 {
-int x;
+	int x;
 
-if (size <= 0)
-_putchar('\n');
-else
-{
-for (x = 0; x < size; x++)
-{
-	for (y = 0; y < size; y++)
+	if (size <= 0)
+		_putchar('\n');
+	else
 	{
-		_putchar(35);
-	}
-_putchar('\n');
-}
-}
+		for (x = 0; x < size; x++)
+		{
+			for (y = 0; y < size; y++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}	
 }
 
--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
 * fizz_buzz - function that prints 1 -100
